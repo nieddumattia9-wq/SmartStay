87b90f04328cb3c6073a616287131298bd6c3df4
Mattia <nieddumattia9@gmail.com>
2026-07-01 18:23:34 +0200

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+/**
+ * SmartStay - Home page
+ * Prima versione del progetto.
+ */
+function App() {
+  return (
+    <main className="app">
+      <h1 className="app__title">SmartStay</h1>
+      <p className="app__subtitle">
+        Find the smartest way to travel.
+      </p>
+    </main>
+  );
+}
+
+export default App;
